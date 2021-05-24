a8f1f65ff1edd2adbc51a22a44bddc2d3ea8b514
Lauren Wash <>
2021-05-24 16:09:13 +0000

    1.1 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,6 +15,10 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+const input = require('readline-sync');
+
+let candidateName = input.question("Enter your name: ")
+
 
 }
 
@@ -38,7 +42,9 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+
+  console.log("Hello, " + candidateName + "!");
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }

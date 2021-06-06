473053e75b0c28a611ccbefd66ef1d3b780f1421
Lauren Wash <>
2021-06-06 17:57:15 +0000

    started part three

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,16 +8,16 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?" ] ;
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = "";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 const input = require('readline-sync');
 
-let candidateName = input.question("Enter your name: ")
+candidateName = input.question("Enter your name: ")
 
 
 }
@@ -25,18 +25,25 @@ let candidateName = input.question("Enter your name: ")
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+for (i = 0; i < questions.length; i++) {
+  const input = require('readline-sync');
+  candidateAnswers = input.question(questions[i])
+}
+
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+
+console.log(`The correct answer to Q#1 was ${correctAnswers[0]}. You answered ${candidateAnswers}.\nThe correct answer to Q#2 was ${correctAnswers[1]}. You answered ${candidateAnswers}.\nThe correct answer to Q#3 was ${correctAnswers[2]}. You answered ${candidateAnswers}.\nThe correct answer to Q#4 was ${correctAnswers[3]}. You answered ${candidateAnswers}.`)
 
 
-  let grade;
+  //let grade;
   
 
-  return grade;
+  //return grade;
 }
 
 function runProgram() {
@@ -48,6 +55,12 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
+
+
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
